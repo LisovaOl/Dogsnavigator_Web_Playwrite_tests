8152fa488cf146c23deb734cdf60b9c884478851
Olena <lisova.olenka@gmail.com>
2026-01-30 13:57:43 +0200

    timeout 90000

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,8 @@ const env = process.env.ENV || "dev";
 dotenv.config({ path: `.env.${env}` });
 console.log("ENV:", env);
 console.log("BASE_URL:", process.env.BASE_URL);
+const isCI = !!process.env.CI;
+
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
@@ -26,6 +28,13 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
+  // ⏱️ ОСЬ ТУТ ТАЙМАУТИ
+  timeout: isCI ? 90_000 : 30_000, // timeout одного тесту
+  expect: {
+    timeout: isCI ? 20_000 : 5_000, // expect(...)
+  },
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -33,6 +42,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     baseURL: process.env.BASE_URL,
+    navigationTimeout: isCI ? 60_000 : 30_000,
+    actionTimeout: isCI ? 20_000 : 10_000,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     video: "retain-on-failure",
