77d0821c0468737b9b83ade612b4e9d7a2a4b175
Olena <lisova.olenka@gmail.com>
2026-01-30 19:38:03 +0200

    Final version

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -27,43 +27,10 @@ export class LoginPage {
   // Perform login
   async login(phone: string, password: string): Promise<void> {
     await this.phoneInput.fill(phone);
-    await this.page.waitForTimeout(300);
-    //await expect(this.phoneInput).toHaveValue(phone);
     await this.passwordInput.fill(password);
-    await this.page.waitForTimeout(300);
-    // await expect(this.passwordInput).toHaveValue(password);
     await this.submitButton.click();
 
     // форма логіну має зникнути
-    //await expect(this.submitButton).toBeHidden({ timeout: 60_000 });
-
-    // Пости мають бути видимі
-    //await expect(this.getByText).toBeVisible();
-    // чекати, що зʼявилась кнопка/таб "Рекомендовані"
-    // await this.page.getByRole("button", { name: "Рекомендовані" }).waitFor({
-    //   state: "visible",
-    //   timeout: 60_000,
-    // });
-    // await Promise.all([
-    //   this.submitButton.click(),
-
-    //   this.page.waitForLoadState("domcontentloaded"),
-    // ]);
-
-    // // ✅ чекати маркер успіху (вибери реальний елемент)
-    // await this.page
-    //   .locator("li.post, .create-post, nav.sidebar")
-    //   .first()
-    //   .waitFor({
-    //     state: "visible",
-    //     timeout: 60_000,
-    //   });
-
-    // // ❗ якщо все ще на login — явно падаємо з зрозумілим текстом
-    // if (this.page.url().includes("/login")) {
-    //   throw new Error(
-    //     `Login did not complete. Still on login page: ${this.page.url()}`,
-    //   );
-    // }
+    await expect(this.submitButton).toBeHidden({ timeout: 60_000 });
   }
 }
