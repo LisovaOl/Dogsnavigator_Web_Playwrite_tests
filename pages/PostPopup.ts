97b50726edd99a1bb0b2a4c531da12fbe7c48ad9
LisovaOl <lisova.olenka@gmail.com>
2026-02-01 08:41:03 +0200

    postPopup Page

---
+++ pages/PostPopup.ts
@@ -0,0 +1,45 @@
+import { Locator, Page, expect } from "@playwright/test";
+
+export class PostPopup {
+  readonly root: Locator;
+
+  readonly creatorName: Locator;
+  readonly editBtn: Locator;
+  readonly deleteBtn: Locator;
+  readonly postImage: Locator;
+  readonly postText: Locator;
+  readonly likeButton: Locator;
+  readonly commentContainer: Locator;
+  readonly commentInputField: Locator;
+  readonly commentSendButton: Locator;
+  readonly closePopupButton: Locator;
+
+  constructor(root: Locator) {
+    this.root = root;
+
+    this.creatorName = root.locator(".creator-dog-info");
+    this.editBtn = root.locator(".edit-btn");
+    this.deleteBtn = root.locator(".delete-btn");
+    this.postImage = root.locator(".post-image");
+    this.postText = root.locator(".post-text");
+    this.likeButton = root.getByRole("button", { name: " Подобається " });
+    this.commentContainer = root.locator(".comment-input-container");
+    this.commentInputField = root.getByRole("textbox", {
+      name: "Залиште коментар",
+    });
+    this.commentSendButton = root.locator(".send-btn");
+    this.closePopupButton = root.locator(".close-btn");
+  }
+
+  async getAuthorName(): Promise<string> {
+    return (await this.creatorName.textContent())?.trim() ?? "";
+  }
+
+  async like(): Promise<void> {
+    await this.likeButton.click();
+  }
+
+  async expectTextContains(text: string) {
+    await expect(this.postText).toContainText(text);
+  }
+}
