3b8159ebb78d2323f785b31075dde6b5abd8718f
LisovaOl <lisova.olenka@gmail.com>
2026-02-08 10:18:19 +0200

    /tests/e2e/temp-test

--- tests/e2e/with-auth/log-in-fixture.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from "../../fixtures/auth.fixture";
-
-test("Posts page is visible after login", async ({ page }) => {
-  await expect(page.locator("h1")).toHaveText("Пости");
-});
